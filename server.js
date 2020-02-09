1afba73bed3dc3f7b587bba68ca097f08a661bf8
Jake Clarkson <jacobwclarkson@gmail.com>
2020-02-09 21:21:06 +0000

    try/catch

--- server.js
+++ server.js
@@ -65,34 +65,39 @@ const s = http.createServer(async (req, res) => {
             res.writeHead(200, { 'Content-Type': 'text/html' })
             let response = `<!doctype html><html><head><title>List persons</title><style>* { font-family: arial; font-size: 40px }</style></head><body><table><tr><th>ID</th><th>Name</th><th>Twitter Handle</th><th>Favourite Language</th></tr>`;
 
-            const results = await db.getAllPersons();
+            try {
+                const results = await db.getAllPersons();
 
-            results.forEach(person => {
-                response += `<tr>
-                    <td>${person.id}</td>
-                    <td>${person.name}</td>
-                    <td>${person.twitter}</td>
-                    <td>${person.lang}</td>
-                    <td><button onclick="deletePerson(${person.id})">Delete</button></td>
-                </tr>`;
-            });
+                results.forEach(person => {
+                    response += `<tr>
+                        <td>${person.id}</td>
+                        <td>${person.name}</td>
+                        <td>${person.twitter}</td>
+                        <td>${person.lang}</td>
+                        <td><button onclick="deletePerson(${person.id})">Delete</button></td>
+                    </tr>`;
+                });
 
-            response += '</table>';
-            response += `
-            <script>
-            function deletePerson(id) {
-                fetch('http://localhost:8888/person', {
-                    method: 'delete',
-                    body: JSON.stringify({ id })
-                }).then(res => {
-                    window.location.reload();
-                }).catch(err => console.error(err));
+                response += '</table>';
+                response += `
+                <script>
+                function deletePerson(id) {
+                    fetch('http://localhost:8888/person', {
+                        method: 'delete',
+                        body: JSON.stringify({ id })
+                    }).then(res => {
+                        window.location.reload();
+                    }).catch(err => console.error(err));
+                }
+                </script>`;
+
+                response += '</body></html>';
+
+                return res.end(response);
+            } catch (e) {
+                res.writeHead(500);
+                return res.end();
             }
-            </script>`;
-
-            response += '</body></html>';
-
-            return res.end(response);
         case '/favicon.ico':
             res.writeHead(200);
             return res.end();
@@ -105,13 +110,23 @@ const s = http.createServer(async (req, res) => {
 
                 switch (req.method) {
                 case 'DELETE':
-                    const success = await db.deletePerson(body.id);
-                    res.writeHead(200);
+                    try {
+                        const success = await db.deletePerson(body.id);
+                        res.writeHead(200);
+                    } catch (e) {
+                        res.writeHead(500);
+                    }
+
                     return res.end();
                 case 'POST':
-                    const id = await db.savePerson(body.name, body.twitterHandle, body.favouriteLanguage);
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    return res.end(JSON.stringify({ id }));
+                    try {
+                        const id = await db.savePerson(body.name, body.twitterHandle, body.favouriteLanguage);
+                        res.writeHead(201, { 'Content-Type': 'application/json' });
+                        return res.end(JSON.stringify({ id }));
+                    } catch (e) {
+                        res.writeHead(500);
+                        return res.end();
+                    }
                 }
             });
         default:
